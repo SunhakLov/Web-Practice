8dfd504a8380e103447636f6887114d195021a25
Sunhak Lov <sunhaklov04@gmail.com>
2025-07-26 19:54:41 -0700

    Build IG UI and use javescript to render all post

--- Instagram Clone/index.js
+++ Instagram Clone/index.js
@@ -17,7 +17,7 @@ const posts = [
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
-        {
+    {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
@@ -28,3 +28,36 @@ const posts = [
     }
 ]
 
+const postHTML = document.querySelector(".post");
+
+function render() {
+    let post = '';
+
+    for (let i = 0; i < posts.length; i++) {
+        post += `<section class="post-card container">
+        <div class="post-profile">
+            <img src="${posts[i].avatar}" alt="${posts[i].name} Profile picture" width="34px" class="profile">
+            <div class="artist-info">
+                <p class="user-name">${posts[i].name}</p>
+                <p class="user-info">${posts[i].location}</p>
+            </div>
+        </div>
+        <div class="post-image">
+            <img src="${posts[i].post}" alt="" width="375px">
+        </div>
+        <div class="interact-container">
+            <div class="interaction-section">
+                <img src="images/icon-heart.png" alt="" width="23px">
+                <img src="images/icon-comment.png" alt="" width="23px">
+                <img src="images/icon-dm.png" alt="" width="23px">
+            </div>
+            <p class="likes">${posts[i].likes} likes</p>
+            <p class="comment"><span>${posts[i].username}</span> ${posts[i].comment}</p>
+        </div>
+    </section>`
+    }
+
+    postHTML.innerHTML = post;
+}
+
+render()
